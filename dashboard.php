70be39e2433fe416d6dd03774fc7d1583767d173
BitBrain-aries <amruiqbalbismillah@gmail.com>
2025-06-18 21:19:21 +0700

    Menambahkan Kunci untuk menambahkan fitur update dan hapus

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['username'])) {
-   
+    // User is already logged in, redirect to welcome page  
     header("Location: login.php");
 
     exit();
@@ -9,12 +9,18 @@ if (!isset($_SESSION['username'])) {
 }
 
 $username = $_SESSION['username'];
+
+// Buat nama file untuk menyimpan jumlah login per user
 $file = "login_count_{$username}.txt";
+
+// Cek apakah file sudah ada, jika ya ambil isinya, kalau belum mulai dari 0
 if (file_exists($file)) {
     $count = (int)file_get_contents($file);
 } else {
     $count = 0;
 }
+
+// Tambah 1 setiap kali halaman dibuka
 $count++;
 
 // Simpan kembali ke file
@@ -31,6 +37,21 @@ if(isset($_POST["nama"]) && isset($_POST["umur"])){
     ];
 
     $_SESSION["daftar"][]=$daftar;
+}
+
+$data_daftar = [
+    "nama" => "",
+    "umur" => "",
+];
+
+$target = "dashboard.php";
+if(isset($_GET["index"])){
+    // disini dianggap terjadi proses update
+    $target = "update.php?index=" . $_GET["index"];
+    if($_GET["index"] != null) {
+        $index = $_GET["index"];
+        $data_daftar = $_SESSION["daftar"][$index];
+    }
 }
 
 ?>
@@ -82,18 +103,18 @@ if(isset($_POST["nama"]) && isset($_POST["umur"])){
     </head>
     <body>
         <h1><?php echo "Selamat datang " . $username . " ke-" . $count  ; ?></h1>
-            <form action="dashboard.php" method="post">
+            <form action="<?php echo $target; ?>" method="post">
          <table>
             <tr>
                 <td colspan="2" style="text-align: center;" >DAFTAR</td>
             </tr>
             <tr>
                 <td>Nama</td>
-                <td><input type="text" name="nama" /></td>
+                <td><input type="text" name="nama" value="<?php echo $data_daftar["nama"] ?>" /></td>
             </tr>
             <tr>
                 <td>Umur</td>
-                <td><input type="number" name="umur" /></td>
+                <td><input type="number" name="umur" value="<?php echo $data_daftar["umur"] ?>"/></td>
             </tr>
             <tr>
                 <td colspan="2" style="text-align: center;">
@@ -104,31 +125,34 @@ if(isset($_POST["nama"]) && isset($_POST["umur"])){
                 </td>
             </tr>
         </table>
-        <table border "1">
+        <table border="1">
             <tr>
                 <td>Nama</td>
                 <td>Umur</td>
-                <td>keterangan</td>
+                <td>Keterangan</td>
+                <td>Aksi</td>
             </tr>
-            
-                <?php foreach($_SESSION["daftar"] as $daftar): ?>
+                <?php foreach($_SESSION["daftar"] as $index => $daftar): ?>
                  <tr>
                     <td><?php echo $daftar["nama"] ?></td>
                     <td><?php echo $daftar["umur"] ?></td>
                     <td><?php
-                    if ($daftar["umur"] < 20){
-                        echo "Remaja";        
-                    }elseif($daftar["umur"] >= 20 && $dafrar_item["umur"] < 40){
-                        echo "Dewasa";
-                    }elseif($daftar["umur"] >= 40){
-                        echo "Tua";
-                    }else{
-                        echo "tidak diketahui"; // INI TIDAK PERNAH TERPANGGIL
-                    }
-                    ?></td>
+                            if($daftar["umur"] < 20){
+                                echo "Remaja";
+                            }elseif($daftar["umur"] >= 20 && $daftar["umur"] < 40){
+                                echo "Dewasa";
+                            }elseif($daftar["umur"] >= 40){
+                                echo "tua";
+                            }else{
+                                echo "Tidak Diketahui";
+                            }
+                        ?>
+                    </td>
+                    <td>
+                        <a href="hapus.php?index=<?php echo $index; ?>">hapus</a> <a href="dashboard.php?index=<?php echo $index; ?>">ubah</a>
+                    </td>
                  </tr>
                  <?php endforeach; ?>
-
         </table>
         </form>
     </body>
